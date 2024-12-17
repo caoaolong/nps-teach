4b0e394f2751cdf355703fa0aae9d6bac2e62fe2
caoaolong <eason105cc@163.com>
2024-12-17 22:14:47 +0800

    初始化项目

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 ﻿#include <stdio.h>
-#include <windows.h>
-#include <wpcapi.h>
+#include <stdlib.h>
+#define HAVE_REMOTE
+#include "pcap.h"
 
-int main(void) {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+
+    pcap_if_t *all_devices;
+    int i=0;
+    char eb[PCAP_ERRBUF_SIZE];
+    if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, nullptr, &all_devices, eb) == -1)
+    {
+        fprintf(stderr,"Error in pcap_findalldevs_ex: %s\n", eb);
+        exit(1);
+    }
+
+    for(const pcap_if_t *device = all_devices; device != NULL; device = device->next)
+    {
+        printf("%d. %s", ++i, device->name);
+        if (device->description)
+            printf(" (%s)\n", device->description);
+        else
+            printf(" (No description available)\n");
+    }
+
+    if (i == 0)
+    {
+        printf("\nNo interfaces found! Make sure WinPcap is installed.\n");
+        return 1;
+    }
+
+    pcap_freealldevs(all_devices);
+
     return 0;
 }
